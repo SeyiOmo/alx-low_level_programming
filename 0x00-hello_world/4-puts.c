8471ff71619c9d3a88ba00992eaee79882173135
SeyiOmo <omoniyiseyi21@gmail.com>
2023-02-19 09:40:48 -0800

    Task 4 1st C program using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a  C program that prints a sentence a nd a new line using the puts function
+ * Return: 0 (Code ran succesfully)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
