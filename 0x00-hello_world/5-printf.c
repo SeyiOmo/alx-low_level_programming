6b88f792432f1281c9fe232f0852f8fa0f79104a
SeyiOmo <omoniyiseyi21@gmail.com>
2023-02-19 10:29:04 -0800

    Task 5 updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,6 @@
  * main - C program that prints a sentence & line using printf
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
